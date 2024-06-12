d09975494495f6fae74bfc005462ad38edde5e7d
Mohammed Misri <mdmisri586@gmail.com>
2024-06-12 14:53:16 +0530

    banners

--- src/ProcessBanner.js
+++ src/ProcessBanner.js
@@ -1,6 +1,6 @@
-// ProcessBanner.js
 import React from 'react';
 import './ProcessBanner.css'; // Importing the CSS file
+import processImage from './data process.jpg'; // Correct import path
 
 const ProcessBanner = () => {
   return (
@@ -11,18 +11,20 @@ const ProcessBanner = () => {
             It's a <span className="highlight-text">simple</span> process!
           </h1>
           <ol className="process-steps">
-            <li><strong>Answer simple questions</strong></li>
-            <li>Our expert lawyers draft your will</li>
-            <li>Receive the final draft and suggest any changes</li>
-            <li>We help you get it Notarised/Registered in your city</li>
+            <ul><strong>Answer simple questions</strong></ul>
+            <ul>Our expert lawyers draft your will</ul>
+            <ul>Receive the final draft and suggest any changes</ul>
+            <ul>We help you get it Notarised/Registered in your city</ul>
           </ol>
           <button className="process-banner-button">Join the waitlist!</button>
         </div>
-        {/* <img 
-          src="path-to-your-image.png" 
-          alt="Process illustration" 
-          className="process-banner-image" 
-        /> */}
+        <div className="process-banner-image-container">
+          <img 
+            src={processImage} 
+            alt="Process illustration" 
+            className="process-banner-image" 
+          />
+        </div>
       </div>
     </div>
   );
